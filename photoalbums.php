a8ae4a688de103ddc7415723ea1b1cc1fc691a27
Tony-mon <tonio@inbox.ru>
2014-10-02 16:01:31 +0400

    Правки

---
+++ photoalbums.php
@@ -0,0 +1,419 @@
+<?include 'include/headerInner.php';?>
+	
+	<form class="filter">
+		
+		<div class="formRow countryOnPage">
+			<span class="filterTitle">Найти альбом</span>
+		</div>
+		<div class="formRow year">
+			<div class="formRow">
+				<label for="" class="label">Год с</label>
+				<select name="" id="">
+					<option value="">2001</option>
+					<option value="">2001</option>
+					<option value="">2001</option>
+					<option value="">2001</option>
+					<option value="">2001</option>
+					<option value="">2001</option>
+				</select>
+			</div>
+			<div class="formRow">
+				<label for="" class="label">по</label>
+				<select name="" id="">
+					<option value="">2001</option>
+					<option value="">2001</option>
+					<option value="">2001</option>
+					<option value="">2001</option>
+					<option value="">2001</option>
+					<option value="">2001</option>
+				</select>
+			</div>
+			
+		</div>
+		<div class="formRow format">
+			<label for="" class="label">Формат</label>
+			<select name="" id="">
+				<option value="">Lorem ipsum.</option>
+				<option value="">Voluptates, consectetur?</option>
+				<option value="">Debitis, aspernatur.</option>
+				<option value="">Dicta, odit.</option>
+				<option value="">Blanditiis, fuga.</option>
+				<option value="">Nulla, mollitia.</option>
+				<option value="">Cum, magni.</option>
+				<option value="">Iusto, expedita.</option>
+				<option value="">Nulla, ipsa.</option>
+				<option value="">Repellat, esse.</option>
+			</select>
+		</div>
+		
+		<div class="formRow inputFormRow">
+			<input type="submit" class="red" value="Найти">
+		</div>
+
+	</form>
+
+	<div class="squerRows">
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya2.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya3.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya2.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya3.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya2.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya3.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya2.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya3.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya2.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya3.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya2.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya3.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya2.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya3.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a><a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya2.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+		<a href="" class="row">
+			<div class="picWrapper squerEl witeBlack">
+				<div class="pic cropped">
+					<div class="inner">
+						<img src="i/meropriyatiya3.jpg" alt="">
+					</div>	
+				</div>
+				<span class="graf plus"><i class="fa fa-search"></i></span>
+			</div>
+			<span class="text">
+				<span class="title nowrap">Сараево (Босния и Герцоговина)</span>
+				<time datetime="2015-10-21">21.10.15</time>
+			</span>				
+		</a>
+
+		
+	</div>
+
+	<div class="pull-right">
+		<script type="text/javascript">(function() {
+		  if (window.pluso)if (typeof window.pluso.start == "function") return;
+		  if (window.ifpluso==undefined) { window.ifpluso = 1;
+		    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+		    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+		    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+		    var h=d[g]('body')[0];
+		    h.appendChild(s);
+		  }})();</script>
+		Поделиться: <div class="pluso" data-background="transparent" data-options="medium,round,line,horizontal,nocounter,theme=02" data-services="vkontakte,facebook,twitter,"></div>
+	</div>		
+
+	<a href="" class="backLink"><i class="fa fa-long-arrow-left"></i> Вернуться к списку</a>
+
+	<br><br>
+	<ul class="pagination">
+	  <li class="prev"><a href=""></a></li>
+	  <li><a href="">1</a></li>
+	  <li><a href="">2</a></li>
+	  <li class="active"><span>3</span></li>
+	  <li class="next"><a href=""></a></li>
+	</ul>
+
+<?include 'include/footer.php';?>
